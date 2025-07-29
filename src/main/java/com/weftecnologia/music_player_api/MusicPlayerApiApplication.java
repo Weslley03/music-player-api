dc58c388d68ea69455f2b5c1d70484a2d364da31
oweslley03 <wesleymoraeswv03@hotmail.com>
2025-07-29 18:18:49 -0300

    initial commit

---
+++ src/main/java/com/weftecnologia/music_player_api/MusicPlayerApiApplication.java
@@ -0,0 +1,13 @@
+package com.weftecnologia.music_player_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weftecnologia/music_player_api/MusicPlayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weftecnologia.music_player_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
